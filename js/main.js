1e9fffb8d98cbb4ac5fe7b645735bbdc0237b448
Nikita Slipachuk <nikitaslipachuk@gmail.com>
2022-04-30 00:17:29 +0300

    add logic

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ const params = [
 		if( idx === randomNumber) {
 		return el }
 	})
-	
+
 	
 	randomNumber = Math.floor(Math.random() *params[1].toSee.length)
 	const make_toSee = params[1].toSee.filter((el, idx) => {
@@ -100,43 +100,78 @@ const params = [
 		return el }
 	})
 	
-	const message = `Саня, с ДНЕМ РОЖДЕНИЯ. Желаю тебе ${make_main},
-Увидеть ${make_toSee},
-добравшись туда на ${make_transport}.
-Обрести счастье с ${make_happyWhith},
-Зарабатывать много ${make_mony} (примерно 1 000 000 грн),
-Конечно же ${make_doInDota} на миду)
-Прожить минимум ${randomNumberLife} лет, а это ${randomNumberLife * 365} дней,
+	const message1 = `C ДНЕМ РОЖДЕНИЯ. Желаю тебе ${make_main},`
+	const message2 =`Увидеть ${make_toSee},`
+	const message3 =`добравшись туда на ${make_transport}.`
+	const message4 =`Обрести счастье с ${make_happyWhith},`
+	const message5 =`Зарабатывать минимум ${make_mony} (примерно 1 000 000 грн),`
+	const message6 =`Конечно же ${make_doInDota} на миду)`
+	const message7 =`Прожить минимум ${randomNumberLife} лет, а это ${randomNumberLife * 365} дней,
 или ${randomNumberLife * 365 * 24} часов, или ${randomNumberLife * 365 * 24 * 60} минут,
-ну или ${randomNumberLife * 365 * 24 * 60 * 60} секунд.
-	За это время ты можешь успеть ${(randomNumberLife * 365 * 24 * 60 * 60)/ 2.5} раза моргнуть или
-	сыграть ${(randomNumberLife * 365 * 24 * 60) / 2} в турбо)`;
-
+	ну или ${randomNumberLife * 365 * 24 * 60 * 60} секунд.`
+	const message8 =	`За это время ты можешь успеть ${(randomNumberLife * 365 * 24 * 60 * 60)/ 2.5} раза моргнуть `
+	const message9 =`или сыграть ${(randomNumberLife * 365 * 24 * 60) / 2} партий в турбо)`;
 
 const mainBTN = document.getElementById(`mainEl`)
+const mainDiv = document.querySelector("div")
 
-mainBTN.addEventListener(`click`, ()  =>  {
-	document.createElement('div').textContent = message
-	document.body.append(message)
-})
+//console.log(mainDiv)
 
-//console.log(mainBTN)
-
-//const addNewMessag = mainBTN.onclick document.createElement('div').textContent = message)
+let counter = 0;
 
 
-//mainBTN
-//console.log(addNewMessag)
+const message = [message1, message2, message3, message4, message5, message6, message7, message8, message9 ]
 
 
-//addNewMessag.append(message)
 
-//const listOfIngredients = document.getElementById('ingredients')
-//const addNewIngredients = ingredients.map(el => {
-//	const ingredient = document.createElement('li')
-//	ingredient.classList.add('item');
-//	ingredient.textContent = el
-//	return ingredient
-//})
+const onBtnClick = (event, arr) => {
+	
+	const a = arr.filter((el, idx) => {
+		if (idx === counter){
+			return el
+		}
+	})
 
-//listOfIngredients.append(...addNewIngredients)
+	let newDiv = document.createElement('div')
+	newDiv.textContent = a
+	newDiv = mainDiv.append(newDiv)
+
+
+	let btnForNextMessage = document.createElement(`button`)
+	btnForNextMessage.textContent = `Увидеть`
+	mainDiv.append(btnForNextMessage)
+	
+
+	btnForNextMessage.addEventListener(`click`,(event, arr) => {
+		const message = [message1, message2, message3, message4, message5, message6, message7, message8, message9]
+	 			const s = message.filter((el, idx) => {
+	 				if (idx === counter){
+	 					return el
+	 				}
+	 			});
+
+
+				newDiv = document.createElement('div')
+				newDiv.textContent = s
+				newDiv = mainDiv.append(newDiv)
+
+
+				let btnForNextMessage = document.createElement(`button`)
+				btnForNextMessage.textContent = `Увидеть`
+				mainDiv.append(btnForNextMessage)
+				
+				if (counter >= 1) {
+					btnForNextMessage.setAttribute(`disabled`, true)
+					}
+						counter += 1;
+	})
+
+	if (counter >= 0) {
+mainBTN.setAttribute(`disabled`, true)
+}
+	counter += 1;
+}
+
+mainBTN.addEventListener(`click`,(event) =>{onBtnClick(event, message)})
+
+//onBtnClick(event,message)
