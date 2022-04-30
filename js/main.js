45df14c8be75ede0aa50b73d837ceda6f3b7d59d
Nikita Slipachuk <nikitaslipachuk@gmail.com>
2022-04-30 15:24:03 +0300

    ok

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ const params = [
 	'Гейзер Флай, США','Дорога гигантов, Ирландия']
 	},
 	
-	 {transport:['парусной лодке джонка','амфибусе','велорикши','пивном велосипеде',
-	'бамбуковом поезде','санях Монте Тобогган','коко-такси','Rolls Royce 103EX']
+	 {transport:['Парусной лодке Джонка','Амфибусе','Велорикши','Пивном велосипеде',
+	'Бамбуковом поезде','Санях Монте Тобогган','Коко-такси','Rolls Royce 103EX']
 	},
 	
 	 {happyWhith:['любимой девушкой','любимым парнем','корманной киской',
@@ -59,14 +59,17 @@ const params = [
 	{doInDota:['нагибать','сосать','посасывать','ебошить',
 	 'разьебывать','унижать','доминировать']},
 	]
+
 	
-	
-	const randomNumberLife = Math.floor(Math.random() *100);
+	const randomNumberLife = Math.floor(Math.random() * (100 - 50) + 50);
 	let randomNumber = Math.floor(Math.random() *params[0].main.length)
+	const machInDota = (randomNumberLife * 365 * 24 * 60) / 2
 	
 	const make_main = params[0].main.filter((el, idx) => {
 		if( idx === randomNumber) {
-		return el }
+		return document.createElement(`a`)
+		
+	}
 	})
 
 	
@@ -100,30 +103,33 @@ const params = [
 		return el }
 	})
 	
-	const message1 = `C ДНЕМ РОЖДЕНИЯ. Желаю тебе ${make_main},`
-	const message2 =`Увидеть ${make_toSee},`
-	const message3 =`добравшись туда на ${make_transport}.`
-	const message4 =`Обрести счастье с ${make_happyWhith},`
-	const message5 =`Зарабатывать минимум ${make_mony} (примерно 1 000 000 грн),`
-	const message6 =`Конечно же ${make_doInDota} на миду)`
-	const message7 =`Прожить минимум ${randomNumberLife} лет, а это ${randomNumberLife * 365} дней,
+	const message1 = `Желаю тебе ${make_main}, 
+	увидеть ${make_toSee}, добравшись туда на ${make_transport}. 
+	Обрести счастье с ${make_happyWhith}. Зарабатывать минимум ${make_mony} примерно 1 000 000 грн.
+	Ну и конечно же ${make_doInDota} на миду)`;
+
+	const message2 =`Минимум ${randomNumberLife} лет.  A это ${randomNumberLife * 365} дней,
 или ${randomNumberLife * 365 * 24} часов, или ${randomNumberLife * 365 * 24 * 60} минут,
-	ну или ${randomNumberLife * 365 * 24 * 60 * 60} секунд.`
-	const message8 =	`За это время ты можешь успеть ${(randomNumberLife * 365 * 24 * 60 * 60)/ 2.5} раза моргнуть `
-	const message9 =`или сыграть ${(randomNumberLife * 365 * 24 * 60) / 2} партий в турбо)`;
+	ну или ${randomNumberLife * 365 * 24 * 60 * 60} секунд.
+	За это время ты можешь успеть ${(randomNumberLife * 365 * 24 * 60 * 60)/ 2.5} раза моргнуть 
+	или сыграть ${machInDota} партий в турбо).` ;
 
 const mainBTN = document.getElementById(`mainEl`)
 const mainDiv = document.querySelector("div")
 
-//console.log(mainDiv)
+//let btnForNextMessage = document.querySelectorAll(`.counter`)
+//	btnForNextMessage.addEventListener(`click`, (event) => {
+//	let	createBtn = document.createElement(`button`)
+//	createBtn.classList.add(`counter`)
+//	createBtn.textContent = `Увидеть`
+//	mainDiv.append(createBtn)
+// 	counter += 1;
+//	if (counter >= 1) {
+//mainBTN.setAttribute(`disabled`, true)
+//}})
 
 let counter = 0;
-
-
-const message = [message1, message2, message3, message4, message5, message6, message7, message8, message9 ]
-
-
-
+const message = [message1, message2]
 const onBtnClick = (event, arr) => {
 	
 	const a = arr.filter((el, idx) => {
@@ -132,46 +138,63 @@ const onBtnClick = (event, arr) => {
 		}
 	})
 
-	let newDiv = document.createElement('div')
-	newDiv.textContent = a
-	newDiv = mainDiv.append(newDiv)
+	let createDiv = document.createElement('div')
+	createDiv.textContent = a
+	mainDiv.append(createDiv)
+
+	let createHreftosee = document.createElement(`a`)
+	createHreftosee.textContent = make_toSee
+	createHreftosee.setAttribute(`target`, `blank`)
+	createHreftosee.setAttribute(`href`, `https://images.google.com/search?q=${make_toSee}`)
+	mainDiv.appendChild(createHreftosee)
+
+	let createHrefGo = document.createElement(`a`)
+	createHrefGo.textContent = make_transport
+	createHrefGo.setAttribute(`target`, `blank`)
+	createHrefGo.setAttribute(`href`, `https://images.google.com/search?q=${make_transport}`)
+	mainDiv.appendChild(createHrefGo)
 
 
-	let btnForNextMessage = document.createElement(`button`)
-	btnForNextMessage.textContent = `Увидеть`
-	mainDiv.append(btnForNextMessage)
-	
+ 	let	createBtn = document.createElement(`button`)
+	 createBtn.classList.add(`counter`)
+	 createBtn.textContent = `Прожить`
+	mainDiv.append(createBtn)
 
-	btnForNextMessage.addEventListener(`click`,(event, arr) => {
-		const message = [message1, message2, message3, message4, message5, message6, message7, message8, message9]
-	 			const s = message.filter((el, idx) => {
-	 				if (idx === counter){
-	 					return el
-	 				}
-	 			});
+	createBtn.addEventListener(`click`,(event, arr) => {
+			const message = [message1, message2]
+					 const a = message.filter((el, idx) => {
+						 if (idx === counter){
+							 return el
+						 }
+					 });
+		
+		
+					 createDiv = document.createElement('div')
+					 createDiv.textContent = a
+					mainDiv.append(createDiv)
+		
+		
+					let refresh = document.createElement(`button`)
+					refresh.textContent = `Поздравить еще`
+					mainDiv.append(refresh)
+					
+					refresh.addEventListener(`click`, (event, arr) => {
+						return window.location.reload()
+					})
+		
+					//console.log (counter)
 
-
-				newDiv = document.createElement('div')
-				newDiv.textContent = s
-				newDiv = mainDiv.append(newDiv)
-
-
-				let btnForNextMessage = document.createElement(`button`)
-				btnForNextMessage.textContent = `Увидеть`
-				mainDiv.append(btnForNextMessage)
-				
-				if (counter >= 1) {
-					btnForNextMessage.setAttribute(`disabled`, true)
-					}
+					if (counter <= 1 || counter>=2) {
+						createBtn.setAttribute(`disabled`, true)
+						}
 						counter += 1;
-	})
+		})
 
-	if (counter >= 0) {
-mainBTN.setAttribute(`disabled`, true)
-}
+
 	counter += 1;
-}
+	if (counter >= 1) {
+mainBTN.setAttribute(`disabled`, true)
+}}
 
 mainBTN.addEventListener(`click`,(event) =>{onBtnClick(event, message)})
 
-//onBtnClick(event,message)
